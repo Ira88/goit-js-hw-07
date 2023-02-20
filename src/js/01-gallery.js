068188fae32bf2ce8f259eacaea9304ce4cb709a
Ira88 <111458593+Ira88@users.noreply.github.com>
2023-02-20 17:23:36 +0200

    task2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const gallery = document.querySelector(".gallery");
-console.log(gallery)
 const markup = galleryItems.map(({preview, original, description})=> {
 return `<div class="gallery__item">
 <a class="gallery__link" href="${original}">
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const gallery = document.querySelector(".gallery");
+const usage = galleryItems.map(({preview, original, description}) => {
+return `<li class="gallery__item">
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" 
+  alt="${description}" />
+</a>
+</li>`
+}).join('');
+gallery.insertAdjacentHTML("beforeend", usage);
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionPosition: 'bottom',
+});
 console.log(galleryItems);
